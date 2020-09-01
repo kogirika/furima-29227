c92dfc043b49a175c26eeea58209ef1195785566
kogirika <kogirika12@gmail.com>
2020-09-01 17:24:06 +0900

    エラーメッセージを出力、バリデーションを修正

--- app/models/user.rb
+++ app/models/user.rb
@@ -8,7 +8,7 @@ class User < ApplicationRecord
   with_options presence: true do
     validates :nickname
     validates :email,          format:{ with: /\A.+@.+\z/, message: "メールアドレスを入力してください"}
-    validates :encrypted_password,format: { with: /\A(?=.*?[a-z])(?=.*?[\d])[a-z\d]+{6,}\z/i, message: "英数字混合６文字以上で入力してください" }
+    validates :password,       format: { with: /\A(?=.*?[a-z])(?=.*?[\d])[a-z\d]+{6,}\z/i, message: "英数字混合６文字以上で入力してください" }
     validates :firstname,      format: { with: /\A[ぁ-んァ-ン一-龥]/, message: "全角で入力してください"}
     validates :lastname,       format: { with: /\A[ぁ-んァ-ン一-龥]/, message: "全角で入力してください"}
     validates :firstname_kana, format: { with: /\A[ァ-ヶー－]+\z/,message: "全角カナで入力してください" }
