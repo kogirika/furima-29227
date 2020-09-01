1c1a2e84cd99702b5f274b5784cc19d5f3575814
kogirika <kogirika12@gmail.com>
2020-09-01 22:54:07 +0900

    rubocop修正

--- app/models/user.rb
+++ app/models/user.rb
@@ -3,23 +3,14 @@ class User < ApplicationRecord
   # :confirmable, :lockable, :timeoutable, :trackable and :omniauthable
   devise :database_authenticatable, :registerable,
          :recoverable, :rememberable, :validatable
-
-
   with_options presence: true do
     validates :nickname
-    validates :email,          format:{ with: /\A.+@.+\z/, message: "メールアドレスを入力してください"}
-    validates :password,       format: { with: /\A(?=.*?[a-z])(?=.*?[\d])[a-z\d]+{6,}\z/i, message: "英数字混合６文字以上で入力してください" }
-    validates :firstname,      format: { with: /\A[ぁ-んァ-ン一-龥]/, message: "全角で入力してください"}
-    validates :lastname,       format: { with: /\A[ぁ-んァ-ン一-龥]/, message: "全角で入力してください"}
-    validates :firstname_kana, format: { with: /\A[ァ-ヶー－]+\z/,message: "全角カナで入力してください" }
-    validates :lastname_kana,  format: { with: /\A[ァ-ヶー－]+\z/,message: "全角カナで入力してください" }
+    validates :email,          format: { with: /\A.+@.+\z/, message: 'メールアドレスを入力してください' }
+    validates :password,       format: { with: /\A(?=.*?[a-z])(?=.*?[\d])[a-z\d]+{6,}\z/i, message: '英数字混合６文字以上で入力してください' }
+    validates :firstname,      format: { with: /\A[ぁ-んァ-ン一-龥]/, message: '全角で入力してください' }
+    validates :lastname,       format: { with: /\A[ぁ-んァ-ン一-龥]/, message: '全角で入力してください' }
+    validates :firstname_kana, format: { with: /\A[ァ-ヶー－]+\z/, message: '全角カナで入力してください' }
+    validates :lastname_kana,  format: { with: /\A[ァ-ヶー－]+\z/, message: '全角カナで入力してください' }
     validates :birthday
   end
-
-
-
-
-
-
-  
 end
