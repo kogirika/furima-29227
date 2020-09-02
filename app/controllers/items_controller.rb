fac3c991cebaa2d9e88c912bb379909e224be03d
kogirika <kogirika12@gmail.com>
2020-09-02 16:52:48 +0900

    商品出品機能を仮実装
    以下を編集
    ルーティング
    コントローラー
    ビュー

--- app/controllers/items_controller.rb
+++ app/controllers/items_controller.rb
@@ -1,4 +1,25 @@
 class ItemsController < ApplicationController
   def index
   end
+
+  def new
+    @item = Item.new
+  end
+  
+  def create
+    @item = Item.new(items_params)
+    if @item.save
+      redirect_to root_path
+    else
+      render :new
+    end
+  end
+
+  private
+  
+  def items_params
+    params.require(:item).permit(:name, :info, :category, :status, :ship_charge, :ship_from,
+                                  :ship_preparation_day, :price, :image).merge(user_id: current_user.id)
+  end
+
 end
