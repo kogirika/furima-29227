080dfe7644c421cd09287d3b19ef36d9f35500de
kogirika <kogirika12@gmail.com>
2020-09-02 15:31:25 +0900

    itemモデル微修正

--- app/models/item.rb
+++ app/models/item.rb
@@ -2,7 +2,7 @@ class Item < ApplicationRecord
   belongs_to :user
   has_one_attached :image
 
-  with_options presence :true do
+  with_options presence: true do
     validates :name
     validates :info
     validates :category
@@ -13,7 +13,7 @@ class Item < ApplicationRecord
     validates :price, numericality: { only_integer: true,
                                       greater_than: 300, less_than: 9999999,
                                       message: '価格は¥300~¥9,999,999の間で入力してください' },
-                      format: { with: /\A[0-9]+\z/,message: '価格は半角数字で入力してください' }
+                      format: { with: /\A[0-9]+\z/, message: '価格は半角数字で入力してください' }
   end
 
 
