faabf92ee465501474d428e8b8e4c166b3d7fcf6
kogirika <kogirika12@gmail.com>
2020-09-04 18:21:13 +0900

    C:before_action :set_itemを設置
    以下を集約
    @item = Item.find(params[:id])

--- app/controllers/items_controller.rb
+++ app/controllers/items_controller.rb
@@ -1,5 +1,6 @@
 class ItemsController < ApplicationController
   before_action :authenticate_user!, except: [:index, :show]
+  before_action :set_item, only: [:show, :edit]
 
   def index
     @items = Item.all.order("created_at DESC")
@@ -19,11 +20,9 @@ class ItemsController < ApplicationController
   end
 
   def show
-    @item = Item.find(params[:id])
   end
 
   def edit
-    @item = Item.find(params[:id])
   end
 
   def update
@@ -35,6 +34,9 @@ class ItemsController < ApplicationController
     end
   end
 
+  def set_item 
+    @item = Item.find(params[:id])
+  end
 
   private
 
